675decf2120fb6aea24443953e04333d9740b515
Casper2411 <ccaassppeerr2411@gmail.com>
2023-11-10 15:49:32 +0100

    Hello_world

---
+++ src/Hello_world.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hello_world {
+
+    public static void main(String[] args) {
+        //Test
+        System.out.println("Hello world!!!!!");
+        List<Integer> TestList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            TestList.add(i);
+        }
+        for (Integer integer : TestList) {
+            System.out.println(integer);
+        }
+    }
+}
